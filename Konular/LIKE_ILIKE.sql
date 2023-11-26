f3721cb85c745c6143a495603315013b326d77af
İsa Murat Cireli <isamurat233@gmail.com>
2023-11-27 00:08:44 +0300

    second commit

---
+++ Konular/LIKE_ILIKE.sql
@@ -0,0 +1,3 @@
+SELECT * FROM customer
+-- WHERE first_name LIKE 'A%' , (LIKE)'ın büyük küçükk harf duyarlılığı vardır. 
+-- WHERE first_name ILIKE 'A%' , (ILIKE)'ın büyük küçük harf duyarlılığı yoktur.
